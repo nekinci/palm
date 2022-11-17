1e8e592f2cdd7d104ff56c4979774e8e6b43e2c5
Niyazi Ekinci <niyaziekinci5050@gmail.com>
2022-11-17 12:41:01 +0300

    Lexer implementation init.

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Print("hello, palm!")
+
+	var lexer *Lexer = NewLexer("1 + 2")
+	var token Token = lexer.Do()
+
+	for token.Kind != EOFToken {
+		if token.Kind != WhitespaceToken {
+			fmt.Printf("TokenKind: %v, TokenText: %v\n", token.Kind.String(), token.Text)
+		}
+		token = lexer.Do()
+	}
 }
